354af1673f58dee8ba9475c9898d82092ace2775
Umqra <golovoiapklavu@gmail.com>
2016-10-21 02:00:18 +0500

    Implement DoubleComparer.cs
    And cover it with Unit-tests

---
+++ TagsCloudVisualization/Geometry/DoubleComparer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry
+{
+    public static class DoubleComparer
+    {
+        public const double DefaultEpsilon = 1e-9;
+
+        public static bool EqualTo(this double a, double b, double eps = DefaultEpsilon)
+        {
+            return Math.Abs(a - b) < eps;
+        }
+
+        public static bool LessThanOrEqualTo(this double a, double b, double eps = DefaultEpsilon)
+        {
+            return a < b || a.EqualTo(b, eps);
+        }
+
+        public static bool LessThan(this double a, double b, double eps = DefaultEpsilon)
+        {
+            return a < b && !a.EqualTo(b, eps);
+        }
+
+        public static bool GreaterThanOrEqualTo(this double a, double b, double eps = DefaultEpsilon)
+        {
+            return a < b || a.EqualTo(b, eps);
+        }
+
+        public static bool GreaterThan(this double a, double b, double eps = DefaultEpsilon)
+        {
+            return a > b && !a.EqualTo(b, eps);
+        }
+    }
+}
