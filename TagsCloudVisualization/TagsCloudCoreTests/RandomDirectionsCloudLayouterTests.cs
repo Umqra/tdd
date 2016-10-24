5a973d2988e4d4b37bc182898b3104f8a66594b1
Umqra <golovoiapklavu@gmail.com>
2016-10-24 14:43:11 +0500

    RandomSparseCloudLayouter covered with unit tests

--- TagsCloudVisualization/TagsCloudCoreTests/RandomDirectionsCloudLayouterTests.cs
+++ TagsCloudVisualization/TagsCloudCoreTests/RandomDirectionsCloudLayouterTests.cs
@@ -80,21 +80,6 @@ namespace TagsCloudCoreTests
                 .Should().HaveCount(5);
         }
 
-        public static TestCaseData[] OppositeDirectionCases =
-        {
-            new TestCaseData((object)new [] {new Size(2, 2), new Size(2, 2), new Size(2, 2)}),
-            new TestCaseData((object)new [] {new Size(3, 1), new Size(1, 3), new Size(2, 4)}),
-        };
-        [TestCaseSource(nameof(OppositeDirectionCases))]
-        public void SecondAndThirdRectangles_LocatedInOppositeDirections(Size[] rectangleSizes)
-        {
-            var rectangles = new List<Rectangle>();
-            foreach (var size in rectangleSizes)
-                rectangles.Add(Layouter.PutNextRectangle(size));
-            Point secondDirection = rectangles[1].Center - rectangles[0].Center;
-            Point thirdDirection = rectangles[2].Center - rectangles[0].Center;
-            Math.Abs(secondDirection.AngleTo(thirdDirection)).Should().BeGreaterThan(Math.PI / 2);
-        }
 
         [TestCase(10)]
         [TestCase(20)]
@@ -108,18 +93,6 @@ namespace TagsCloudCoreTests
                 .BeGreaterThan(boundingBox.Size.Height / 2)
                 .And
                 .BeLessThan(boundingBox.Size.Height * 2);
-        }
-
-        [TestCase(50)]
-        public void Rectangles_ShouldOccupyMostOfFreeSpace(int numberOfRectangles)
-        {
-            IEnumerable<Point> allCorners = Enumerable.Empty<Point>();
-            double rectanglesArea = numberOfRectangles;
-            for (int i = 0; i < numberOfRectangles; i++)
-                allCorners = allCorners.Concat(Layouter.PutNextRectangle(new Size(1, 1)).Corners);
-            Rectangle boundingBox = Rectangle.BoundingBoxOf(allCorners);
-
-            rectanglesArea.Should().BeGreaterThan(boundingBox.Area / 2);
         }
 
         [TearDown]
@@ -158,6 +131,60 @@ namespace TagsCloudCoreTests
             Layouter = new RandomDirectionsCloudLayouter(center);
             var rectangle = Layouter.PutNextRectangle(rectangleSize);
             rectangle.Should().Match<Rectangle>(r => r.Contains(center));
+        }
+
+        [TestCase(50)]
+        public void Rectangles_ShouldOccupyMostOfFreeSpace(int numberOfRectangles)
+        {
+            IEnumerable<Point> allCorners = Enumerable.Empty<Point>();
+            double rectanglesArea = numberOfRectangles;
+            for (int i = 0; i < numberOfRectangles; i++)
+                allCorners = allCorners.Concat(Layouter.PutNextRectangle(new Size(1, 1)).Corners);
+            Rectangle boundingBox = Rectangle.BoundingBoxOf(allCorners);
+
+            rectanglesArea.Should().BeGreaterThan(boundingBox.Area / 2);
+        }
+    }
+
+    [TestFixture]
+    public class RandomSparseCloudLayouterTests : CloudLayouterTests
+    {
+        public override ICloudLayouter Layouter { get; set; }
+        public override int ScaleFactor => 100;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Layouter = new RandomSparseCloudLayouter(new Point(0, 0));
+        }
+
+        public static TestCaseData[] FirstRectangleCases =
+        {
+            new TestCaseData(new Point(0, 0), new Size(2, 2)),
+            new TestCaseData(new Point(10, 10), new Size(2, 2)),
+        };
+        [TestCaseSource(nameof(FirstRectangleCases))]
+        public void FirstRectangle_ShouldContainCenter(Point center, Size rectangleSize)
+        {
+            Layouter = new RandomSparseCloudLayouter(center);
+            var rectangle = Layouter.PutNextRectangle(rectangleSize);
+            rectangle.Should().Match<Rectangle>(r => r.Contains(center));
+        }
+
+        public static TestCaseData[] OppositeDirectionCases =
+        {
+            new TestCaseData((object)new [] {new Size(2, 2), new Size(2, 2), new Size(2, 2)}),
+            new TestCaseData((object)new [] {new Size(3, 1), new Size(1, 3), new Size(2, 4)}),
+        };
+        [TestCaseSource(nameof(OppositeDirectionCases))]
+        public void SecondAndThirdRectangles_LocatedInOppositeDirections(Size[] rectangleSizes)
+        {
+            var rectangles = new List<Rectangle>();
+            foreach (var size in rectangleSizes)
+                rectangles.Add(Layouter.PutNextRectangle(size));
+            Point secondDirection = rectangles[1].Center - rectangles[0].Center;
+            Point thirdDirection = rectangles[2].Center - rectangles[0].Center;
+            Math.Abs(secondDirection.AngleTo(thirdDirection)).Should().BeGreaterThan(Math.PI / 2);
         }
     }
 }
